3064c9b8315c6b7ef69bc0b47eba00993b9ebd2b
jonneschreuder <jonne.gabrielle@live.nl>
2026-06-20 21:56:56 +0200

    Update home.js
    lyrics added

--- home.js
+++ home.js
@@ -14,56 +14,37 @@ function newSong(title, key, level, instrument, melody) {
     return song;
 }
 
-const scale = [
-  {normal: "do", sharp: "di"},
-  {normal: "re", sharp: "ri", flat: "ra"},
-  {normal: "mi", flat: "ma"},
-  {normal: "fa", sharp: "fi"},
-  {normal: "so", sharp: "si", flat: "sa"},
-  {normal: "la", sharp: "li", flat: "lo"},
-  {normal: "ti", flat: "ta"}
-];
 
-function calcChromDegree(absDeg, acc) {
-  let chromDeg = 2 * absDeg - Math.floor(absDeg / 7) - Math.floor((absDeg + 4) / 7);
-  if (acc === "sharp") {chromDeg++};
-  if (acc === "flat") {chromDeg--};
-  return chromDeg;
-}
-
-function newItem(song, type) {
-    song.melody.push({type: type, id: id});
-    id++;
-}
-
-
-const shape = newSong("Shape of you - Ed Sheeran", "g", 1, "piano", 
+newSong("Shape of you - Ed Sheeran", "g", 1, "piano", 
   [5, -1, -1, 7, -1, -1, 5, -1,
   5, -1, -1, 7, -1, -1, 5, -1, 
   5, -1, -1, 7, -1, -1, 5, -1, 
   6, -1, -1, 5, -1, -1, 4, -1]
 );
 
-const beethoven = newSong("Beethovens 5th Symphony", "g", 1, "piano", 
+newSong("Beethovens 5th Symphony", "g", 1, "piano", 
   [9, 9, 9, 7,
   -1, -1, -1, -1, -1, -1, -1, -1,
   8, 8, 8, 6]
 );
 
-const mario = newSong("Super Mario Bros tune", "d", 3, "piano",
-  [9, 9, -1, 9, -1, 7, 9, -1, 11, -1, -1, -1, 4]
+newSong("Super Mario Bros tune", "d", 3, "piano",
+  [9, 9, -1, 9, -1, 7, 9, -1, 11, -1, -1, -1, 4, -1, -1, -1, 
+  7, -1, -1, 4, -1, -1, 2, -1, -1, 5, -1, 6, -1, {diaDeg: 6, acc: "flat"}, 5, -1, 
+  4, 9, -1, 11, 12, -1, 10, 11, -1, 9, -1, 7, 8, 6
+  ]
 );
 
-const nokia = newSong("Nokia ringtone", "a", 2, "piano",
+newSong("Nokia ringtone", "a", 2, "piano",
   [11, 10, 5, -1, 6, -1, 9, 8, 3, -1, 4, -1, 8, 7, 2, -1, 4, -1, 7]
 )
 
-const somebody = newSong("Somebody that I used to know - Gotye", "f", 2, "piano",
+newSong("Somebody that I used to know - Gotye", "f", 2, "piano",
   [4, -1, 4, -1, 8, -1, 8, -1, 9, 10, 11, 9, 8, -1, -1, -1, 
     7, -1, 7, -1, 6, -1, 6, -1, 5, -1, 5, -1, 4]
 )
 
-const starWars = newSong("Star Wars theme song", "a# / b♭", 2, "piano",
+newSong("Star Wars theme song", "a# / b♭", 2, "piano",
   [7, -1, -1, -1, -1, -1, 11, -1, -1, -1, -1, -1, 
     10, 9, 8, 14, -1, -1, -1, -1, -1, 11, -1, -1, 
     10, 9, 8, 14, -1, -1, -1, -1, -1, 11, -1, -1, 
@@ -71,15 +52,13 @@ const starWars = newSong("Star Wars theme song", "a# / b♭", 2, "piano",
   ]
 )
 
-const heart = newSong("My heart will go on", "e", 1, "piano",
-  [7, -1, -1, -1, -1, -1, -1, -1, 8, -1, -1, -1, -1, -1, 4, -1,
-    11, -1, -1, -1, 10, -1, 9, 8
+newSong("My heart will go on", "e", 1, "piano",
+  [{diaDeg: 7, lyric: "Near,"}, -1, -1, -1, -1, -1, -1, -1, 
+  {diaDeg: 8, lyric: "far,"}, -1, -1, -1, -1, -1, {diaDeg: 4, lyric: "where -"}, -1,
+  {diaDeg: 11, lyric: "e -"}, -1, -1, -1, {diaDeg: 10, lyric: "ver"}, -1, {diaDeg: 9, lyric: "you"}, {diaDeg: 8, lyric: "are"}
   ]
 )
 
-
-
-//DEZE CONSTRUCTIE WERKT ALLEEN IN DE LIVE SERVER, NIET VANAF FILES
 
 
 const generatorPopup = document.getElementById("generator-popup");
